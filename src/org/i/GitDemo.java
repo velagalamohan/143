6b7b7b0ba84a2a1107e7e013ba64a9095ecdb07b
velagalamohan <velagalamohan@gmail.com>
2021-12-26 14:43:17 +0530

    include div

--- src/org/i/GitDemo.java
+++ src/org/i/GitDemo.java
@@ -4,6 +4,9 @@ public class GitDemo {
 public void add() {
 System.out.println("we have to add");
 }
+public void div() {
+System.out.println("div all");
+}
 public void mul() {
 System.out.println("we have to mult");
 }
