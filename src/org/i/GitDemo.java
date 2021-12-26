f6c92da3fc4be698925eb585ceafc447c7487ddb
velagalamohan <velagalamohan@gmail.com>
2021-12-26 14:47:22 +0530

    include substract

--- src/org/i/GitDemo.java
+++ src/org/i/GitDemo.java
@@ -4,6 +4,9 @@ public class GitDemo {
 public void add() {
 System.out.println("we have to add");
 }
+public void sub() {
+System.out.println("sub all values");
+}
 public void mul() {
 System.out.println("we have to mult");
 }
